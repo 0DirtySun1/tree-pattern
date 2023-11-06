c1e9aaee6bfd09119f9310e61b5979cb477ff5c9
0DirtySun1 <bonbonrinsaal@web.de>
2023-11-06 10:34:54 +0100

    added christmas tree balls

--- tree_pattern.py
+++ tree_pattern.py
@@ -7,24 +7,21 @@ columns = int(input("Wie viele Spalten: "))
 tree_floors = int(input("Wie viele Abstufungen: "))
 rows_half = round(rows/2)
 columns_half = ceil(columns/2)
-star_bell = ["* ", "o "]
+star_bell = ["* ", "$ ", "* "]
 
 def tree_pattern(rows, columns):
     stars = ""
-    x = ""
+    x = list()
     for j in range(0, tree_floors):
         rows_floor = round(rows/(tree_floors-j))
         if j < 1:
             for i in range(1, rows_floor):
-                stars = "* " * round(((100/rows)/100)*columns*i)
-                if stars > "* " * columns:
-                    print("* " * columns)
-                else:
-                    print(stars.center(columns*2," "))    
-                        
+                for k in range(round(((100/rows)/100)*columns*i)):
+                    x.append(random.choice(star_bell))
+                print("".join(x).center(columns*2," "))      
         else:
             for i in range(round(rows_floor/3), rows_floor):
-                stars = "* " * round(((100/rows)/100)*columns*i)
+                stars = "* " * round(((100/rows)/100)*columns*i+1))
                 if stars > "* " * columns:
                     print("* " * columns)
                 else:
