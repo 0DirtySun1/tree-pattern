78725d761186836d35899bbf234cc12461a7731e
0DirtySun1 <bonbonrinsaal@web.de>
2023-11-06 10:44:55 +0100

    added random christmas tree balls final change

--- tree_pattern.py
+++ tree_pattern.py
@@ -7,7 +7,7 @@ columns = int(input("Wie viele Spalten: "))
 tree_floors = int(input("Wie viele Abstufungen: "))
 rows_half = round(rows/2)
 columns_half = ceil(columns/2)
-star_bell = ["* ", "$ ", "* "]
+star_bell = ["* ", "o ", "* "]
 
 def tree_pattern(rows, columns):
     stars = ""
@@ -19,13 +19,14 @@ def tree_pattern(rows, columns):
             for i in range(1, rows_floor):
                 for k in range(round(((100/rows)/100)*columns*i)):
                     x.append(random.choice(star_bell))
-                print("".join(x).center(columns*2," "))      
+                print("".join(x).center(columns*2," "))
+                x.clear()      
         else:
             for i in range(round(rows_floor/3), rows_floor):
                 for k in range(round(((100/rows)/100)*columns*i)):
                     y.append(random.choice(star_bell))
-                print("".join(x).center(columns*2," "))    
-
+                print("".join(y).center(columns*2," "))    
+                y.clear()
     for i in range(0, rows_half+1):
         stars = "* " * (columns_half-1)
         print(stars.center(columns*2," "))
