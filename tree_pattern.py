18a8a154f23099690adc3dbd48c06acbfa0558f7
0DirtySun1 <bonbonrinsaal@web.de>
2023-11-06 11:10:39 +0100

    bug fixes and code improvements

--- tree_pattern.py
+++ tree_pattern.py
@@ -5,12 +5,11 @@ from math import ceil
 rows = int(input("Wie viele Reihen: "))
 columns = int(input("Wie viele Spalten: "))
 tree_floors = int(input("Wie viele Abstufungen: "))
+star_bell = ["* ", "o ", "* "]
 rows_half = round(rows/2)
 columns_half = ceil(columns/2)
-star_bell = ["* ", "o ", "* "]
 
-def tree_pattern(rows, columns):
-    stars = ""
+def tree_pattern(rows, columns, stars):
     x = list()
     y = list()
     for j in range(0, tree_floors):
@@ -23,11 +22,11 @@ def tree_pattern(rows, columns):
                 x.clear()      
         else:
             for i in range(round(rows_floor/3), rows_floor):
-                for k in range(round(((100/rows)/100)*columns*i)):
+                for k in range(round(((100/rows)/100)*columns*(i+1))):
                     y.append(random.choice(star_bell))
                 print("".join(y).center(columns*2," "))    
                 y.clear()
     for i in range(0, rows_half+1):
         stars = "* " * (columns_half-1)
         print(stars.center(columns*2," "))
-tree_pattern(rows, columns)
+tree_pattern(rows, columns,"")
